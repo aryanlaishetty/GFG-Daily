e77acdd07470e47a894927fa5a6e0dba64ac27c0
Aryan Laishetty <129854148+aryanlaishetty@users.noreply.github.com>
2025-12-30 10:59:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.cpp
+++ Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.cpp
@@ -14,62 +14,24 @@ public:
 
 class Solution {
   public:
-    bool rootToNodePath(Node* root, int r, vector<int> &path) {
-        if(root == NULL) {
-            return false;
-        }
-
-        path.push_back(root->data);
-        if(root->data == r) {
-            return true;
-        }
-
-        bool isLeft = rootToNodePath(root->left, r, path); 
-        bool isRight = rootToNodePath(root->right, r, path); 
-
-        if(isLeft || isRight) {
-            return true;
-        }
-
-        path.pop_back();
-        return false;
-    }
-
-    Node* findNode(Node* root, int lca) {
-        if(!root) {
-            return NULL;
-        }
-
-        if(root->data == lca) {
-            return root;
-        }
-
-        Node* left = findNode(root->left, lca);
-
-        if(left) {
-            return left;
-        }
-
-        return findNode(root->right, lca);
-    }
 
     Node* lca(Node* root, int n1, int n2) {
         //  code here
-        vector<int> path1;
-        vector<int> path2;
-
-        rootToNodePath(root, n1, path1);
-        rootToNodePath(root, n2, path2);
-
-        int lca = -1;
-
-        for(int i=0, j=0; i<path1.size() && j<path2.size(); i++,j++) {
-            if(path1[i] != path2[j]) {
-                break;
-            }
-            lca = path1[i];
+        if(root == NULL) {
+            return NULL;
         }
-        
-        return findNode(root, lca);
+
+        if(root->data == n1 || root->data == n2) {
+            return root;
+        }
+
+        Node* leftLCA = lca(root->left, n1, n2);
+        Node* rightLCA = lca(root->right, n1, n2);
+
+        if(leftLCA != NULL && rightLCA != NULL) {
+            return root;
+        }
+
+        return leftLCA == NULL ? rightLCA : leftLCA; 
     }
 };
