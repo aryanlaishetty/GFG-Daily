d7548faea6652559e460faf6fdbbc0e5d3c8dfcf
Aryan Laishetty <129854148+aryanlaishetty@users.noreply.github.com>
2026-02-11 10:48:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    //Optimal Approach
+    //tc = O(m+n) + O(mlogm) + O(nlogn)
+    //sc = O(1)
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        // code here
+        int m = a.size();
+        int n = b.size();
+        int i = m-1;
+        int j = 0;
+        
+        while(i>=0 && j<n) {
+            if(a[i] > b[j]) {
+                swap(a[i], b[j]);
+                i--, j++;
+            } else {
+                break;
+            }
+        }
+        
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+    }
+};
